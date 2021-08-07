2928ac8c3259e077a556ee1e779223782af76873
91954 <190330126@klh.edu.in>
2021-08-07 11:22:28 +0530

    JFSD FIRST commit

---
+++ Junit/src/JUnitClass.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JUnitClass {
+  @Test
+  
+  public void SetUp(){
+    String str = "This is KL University";
+    assertEquals("This is KL University",str);
+    
+  }
+
+  
+
+}
---
+++ Junit/src/methods.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Test;
+
+public class methods {
+   @Test
+    public void setup() {
+      String str = "klh";
+      String str1 = "klh";
+      String str2 = "null";
+      String str3 = "edu";
+      int val = 5;
+      int val1 = 7;
+      String[] expectedArray= {"one","two","three"};
+      String[] resultArray = {"one","two","three"};
+      //assertEquals: checks whether two primitives are same or not
+      assertEquals(str,str1);
+      
+      
+    }
+   @Test
+   public void AsseSame() {
+     String str = "klh";
+     String str1 = "klh";
+     String str2 = "null";
+     assertSame(str, str1);
+   }
+
+}
